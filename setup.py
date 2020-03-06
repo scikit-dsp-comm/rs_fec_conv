2b6e332a74c4025d40015d6e7c96873966993ab0
broepken57 <broepken57@hotmail.com>
2020-03-06 10:37:38 -0700

    Mar 6, 2020

--- setup.py
+++ setup.py
@@ -74,60 +74,60 @@ class PostInstallCommand(install):
         )
 
 
-class CargoModifiedSdist(SdistCommand):
-    """Modifies Cargo.toml to use an absolute rather than a relative path
+# class CargoModifiedSdist(SdistCommand):
+    # """Modifies Cargo.toml to use an absolute rather than a relative path
 
-    The current implementation of PEP 517 in pip always does builds in an
-    isolated temporary directory. This causes problems with the build, because
-    Cargo.toml necessarily refers to the current version of pyo3 by a relative
-    path.
+    # The current implementation of PEP 517 in pip always does builds in an
+    # isolated temporary directory. This causes problems with the build, because
+    # Cargo.toml necessarily refers to the current version of pyo3 by a relative
+    # path.
 
-    Since these sdists are never meant to be used for anything other than
-    tox / pip installs, at sdist build time, we will modify the Cargo.toml
-    in the sdist archive to include an *absolute* path to pyo3.
-    """
+    # Since these sdists are never meant to be used for anything other than
+    # tox / pip installs, at sdist build time, we will modify the Cargo.toml
+    # in the sdist archive to include an *absolute* path to pyo3.
+    # """
 
-    def make_release_tree(self, base_dir, files):
-        """Stages the files to be included in archives"""
-        super().make_release_tree(base_dir, files)
+    # def make_release_tree(self, base_dir, files):
+        # """Stages the files to be included in archives"""
+        # super().make_release_tree(base_dir, files)
 
-        import toml
+        # import toml
 
-        # Cargo.toml is now staged and ready to be modified
-        cargo_loc = os.path.join(base_dir, "Cargo.toml")
-        assert os.path.exists(cargo_loc)
+        # # Cargo.toml is now staged and ready to be modified
+        # cargo_loc = os.path.join(base_dir, "Cargo.toml")
+        # assert os.path.exists(cargo_loc)
 
-        with open(cargo_loc, "r") as f:
-            cargo_toml = toml.load(f)
+        # with open(cargo_loc, "r") as f:
+            # cargo_toml = toml.load(f)
 
-        rel_pyo3_path = cargo_toml["dependencies"]["pyo3"]["path"]
-        base_path = os.path.dirname(__file__)
-        abs_pyo3_path = os.path.abspath(os.path.join(base_path, rel_pyo3_path))
+        # rel_pyo3_path = cargo_toml["dependencies"]["pyo3"]["path"]
+        # base_path = os.path.dirname(__file__)
+        # abs_pyo3_path = os.path.abspath(os.path.join(base_path, rel_pyo3_path))
 
-        cargo_toml["dependencies"]["pyo3"]["path"] = abs_pyo3_path
+        # cargo_toml["dependencies"]["pyo3"]["path"] = abs_pyo3_path
 
-        with open(cargo_loc, "w") as f:
-            toml.dump(cargo_toml, f)
+        # with open(cargo_loc, "w") as f:
+            # toml.dump(cargo_toml, f)
 
 
-class PyTest(TestCommand):
-    user_options = []
+# class PyTest(TestCommand):
+    # user_options = []
 
-    def run(self):
-        self.run_command("test_rust")
+    # def run(self):
+        # self.run_command("test_rust")
 
-        import subprocess
+        # import subprocess
 
-        subprocess.check_call(["pytest", "tests"])
+        # subprocess.check_call(["pytest", "tests"])
 
 
 setup_requires = ["setuptools-rust>=0.10.1", "wheel"]
 install_requires = []
-tests_require = install_requires + ["pytest", "pytest-benchmark"]
+#tests_require = install_requires + ["pytest", "pytest-benchmark"]
 
 setup(
     name="rs_fec_conv",
-    version="0.3.1",
+    version="0.3.4",
 	author="Benjamin Roepken",
 	author_email="broepken57@hotmail.com",
 	url="https://github.com/grayfox57/rs_fec_conv",
@@ -150,5 +150,6 @@ setup(
     setup_requires=setup_requires,
     include_package_data=True,
     zip_safe=False,
-    cmdclass={"install": PostInstallCommand, "bdist_wheel": bdist_wheel, "test": PyTest, "sdist": CargoModifiedSdist},
+    #cmdclass={"install": PostInstallCommand, "bdist_wheel": bdist_wheel, "test": PyTest, "sdist": CargoModifiedSdist},
+	cmdclass={"install": PostInstallCommand, "bdist_wheel": bdist_wheel},
 )
